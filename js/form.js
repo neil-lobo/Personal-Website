6335af829f4a079c012b3476528a82c1e8479d9a
2547technowizz <neil_edward_lobo@hotmail.com>
2021-01-12 21:05:55 -0500

    added progress indicators for form submission
    also added feature to stop multiple form submissions while submitting

--- js/form.js
+++ js/form.js
@@ -1,27 +1,40 @@
+let submitting = false;
+
 document.getElementById("contact-form").addEventListener("submit", (e) => {
 	e.preventDefault();
 
-	let name = document.getElementById("form-name").value;
-	let email = document.getElementById("form-email").value;
-	let message = document.getElementById("form-message").value;
+	if (submitting) return;
+	submitting = true;
+
+	let name = document.getElementById("form-name");
+	let email = document.getElementById("form-email");
+	let message = document.getElementById("form-message");
+
+	document.getElementById("loader").style.display = "block";
 
 	const formData = new FormData();
-	formData.append("name", name);
-	formData.append("email", email);
-	formData.append("message", message);
-
-	document.getElementById("form-name").value = "";
-	document.getElementById("form-email").value = "";
-	document.getElementById("form-message").value = "";
+	formData.append("name", name.value);
+	formData.append("email", email.value);
+	formData.append("message", message.value);
 
 	fetch("https://getform.io/f/88c329ce-387f-4633-8587-dc28db5a425a", {
 		method: "POST",
 		body: formData,
 	})
 		.then((res) => {
-			// name.value = "";
-			// email.value = "";
-			// message.value = "";
+			name.value = "";
+			email.value = "";
+			message.value = "";
+			submitting = false;
+			document.getElementById("loader").style.display = "none";
+			document.getElementById("success").style.opacity = "1";
+			document.getElementById("success").style.display = "flex";
+			setTimeout(() => {
+				document.getElementById("success").style.opacity = "0";
+				setTimeout(() => {
+					document.getElementById("success").style.display = "none";
+				}, 500);
+			}, 5000);
 			console.log(res);
 		})
 		.catch((err) => {
